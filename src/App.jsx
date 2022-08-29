3138f67316b27d6c58a1187ae0e04483c16c006e
Your name here <brkmer@gmail.com>
2022-08-29 12:13:27 -0400

    expensive function addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import App1 from "./App1";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const expensiveFunction = () => {
+    let total = 0;
+    for(let i = 0; i < text.length; i++) {
+      total += i;
+      }
+      return total;
+  };
+
+  console.log("component rendered")
   return <App1 />;
 }
 
